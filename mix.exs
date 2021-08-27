6e6b9af1bdeacdb92465d928b5f581ca348e7b4b
Ricardo Paiva <ricardoccpaiva@gmail.com>
2021-08-27 23:09:40 +0100

    Lower Elixir version to 1.10

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule OpentelemetryTesla.MixProject do
     [
       app: :opentelemetry_tesla,
       version: "0.1.0-rc.2",
-      elixir: "~> 1.12",
+      elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       package: package(),
