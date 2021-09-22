1a72ba74f80c46b0d21f4f49e6537964fa4e0c1d
Ricardo Paiva <ricardoccpaiva@gmail.com>
2021-09-22 14:49:47 +0100

    Update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule OpentelemetryTesla.MixProject do
   def project do
     [
       app: :opentelemetry_tesla,
-      version: "1.1.0-rc.1",
+      version: "1.2.0-rc.1",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
