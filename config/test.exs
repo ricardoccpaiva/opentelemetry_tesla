45724232001c3bcca7a6bfcc5b28931d666804a4
Indrek Juhkam <indrek@urgas.eu>
2021-10-18 16:03:23 +0300

    Fix test errors
    We got
    ```
    10:00:00.917 [warn]  Description: 'Authenticity is not established by certificate path validation'
         Reason: 'Option {verify, verify_peer} and cacertfile/cacerts is missing'
    
    10:00:00.952 [info]  client error exporting spans {:failed_connect,
     [{:to_address, {'localhost', 4317}}, {:inet, [:inet], :econnrefused}]}
    ```
    previously.
    
    I changed the test setup to set up `otel_exporter_pid` immediately which
    seemed to remove exporter related errors/warnings.

--- config/test.exs
+++
@@ -1,6 +0,0 @@
-import Config
-
-config :opentelemetry,
-  sampler: {:otel_sampler_always_on, %{}},
-  tracer: :otel_tracer_default,
-  processors: [{:otel_batch_processor, %{scheduled_delay_ms: 1}}]
